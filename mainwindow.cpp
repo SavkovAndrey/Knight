488c05c4b51bb965a14a96f0ee17bd3439ecc0df
SavkovAndrey <141626360+SavkovAndrey@users.noreply.github.com>
2023-12-15 23:43:18 +0300

    test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,3 +15,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//------------------------------ РАСЧИТАТЬ
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString temp;
+       QTableWidgetItem *item = new QTableWidgetItem();
+
+    item = ui->tableWidget->item(1, 1);
+    if(item != NULL){
+    temp = item->text();
+    }
+
+     // temp = "aaaaaaa";
+     // item->setText("aaaaaaa");
+      ui->tableWidget->setItem(2, 1, item);
+      ui->label_7->setText(temp);
+
+}
+
